c0db1563abf5b60122c076d307fde57717c442e9
joCol-git <joao.r.colaco@gmail.com>
2023-01-17 17:36:25 +0000

    first commit

---
+++ graphql-server/src/index.ts
@@ -0,0 +1,16 @@
+import { createServer } from 'node:http'
+import { createYoga } from 'graphql-yoga'
+import { schema } from './schema'
+ 
+const port = Number(process.env.API_PORT) || 4000
+// Create a Yoga instance with a GraphQL schema.
+const yoga = createYoga({ schema })
+ 
+// Pass it into a server to hook into request handlers.
+const server = createServer(yoga)
+ 
+// Start the server and you're done!
+server.listen(4000, () => {
+  console.info(`🚀 GraphQL Server ready at http://localhost:${port}/graphql`)
+});
+
